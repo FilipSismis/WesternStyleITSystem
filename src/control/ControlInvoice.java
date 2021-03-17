6e77875954b0efb1dfd26da01967f6cf23da4ac1
AmiClerick <63500421+AmiClerick@users.noreply.github.com>
2021-03-17 20:04:17 +0300

    Add files via upload

---
+++ src/control/ControlInvoice.java
@@ -0,0 +1,12 @@
+package control;
+
+import model.*;
+
+public class ControlInvoice {
+private DBInvoice dbinvoice;
+
+	public ControlInvoice() {
+		 // public Invoice createInvoice(int InvoiceNo, double totalPrice, Date paymentDate);
+		
+	}
+}
